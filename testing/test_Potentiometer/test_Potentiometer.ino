97a6d1140e449d0c0a71f3089dcfe696204dba30
Gabriel Sjöberg <ga22be@live.se>
2020-05-02 22:26:37 +0200

    Added untracked test

---
+++ testing/test_Potentiometer/test_Potentiometer.ino
@@ -0,0 +1,16 @@
+int potPin = A0;
+int potVal = 0;
+
+void setup()
+{
+  pinMode(potPin, INPUT);  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  potVal = analogRead(potPin);  
+  Serial.print("Analog value: ");
+  Serial.println(potVal);
+  delay(500); 
+}
